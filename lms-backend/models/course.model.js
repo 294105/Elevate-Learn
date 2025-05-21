96bc9f110397a32814cfdf8411a59fbc12b53c7e
294105 <294105@ust.com>
2025-05-21 09:27:48 +0530

    TOC

--- lms-backend/models/course.model.js
+++ lms-backend/models/course.model.js
@@ -4,6 +4,7 @@ const courseSchema = new mongoose.Schema({
   title: { type: String, required: true },
   description: { type: String },
   category: { type: String },
+  TOC: { type: String ,required: false},
   thumbnail: { type: String }, // could be image URL or file path
   tutor: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
   enrolledStudents: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }],
--- lms-frontend/src/app/courses/create-course/create-course.component.ts
+++ lms-frontend/src/app/courses/create-course/create-course.component.ts
@@ -15,6 +15,7 @@ export class CreateCourseComponent {
   title = '';
   description = '';
   category = '';
+  toc = '';
   thumbnail: File | null = null;
   isPublished = false;
 
@@ -29,6 +30,7 @@ export class CreateCourseComponent {
     formData.append('title', this.title);
     formData.append('description', this.description);
     formData.append('category', this.category);
+    formData.append('toc', this.toc);
     formData.append('isPublished', this.isPublished ? 'true' : 'false');
     if (this.thumbnail) {
       formData.append('thumbnail', this.thumbnail);
